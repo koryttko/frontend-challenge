c74911f0fbe312f6825b475520db638948b5d63d
koryttko <Kim-selina.Koryttko@studmail.w-hs.de>
2023-07-12 22:02:35 +0200

    chore: mobile layout

--- src/lib/components/sections/ClientsSection.svelte
+++ src/lib/components/sections/ClientsSection.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	let headline: string = "You'll be in good company";
+
 	let companyLogosUrl: Array<string> = [
 		'src/lib/assets/images/Google.svg',
 		'src/lib/assets/images/Mailchimp.svg',
@@ -7,9 +9,9 @@
 	];
 </script>
 
-<section class="px-24 py-20">
+<section class="py-12 px-8 md:py-24 md:px-20">
 	<div class="flex flex-col w-full items-center justify-center text-center gap-12">
-		<h3>You'll be in good company</h3>
+		<h2 class="text-4xl">{headline}</h2>
 		<div class="flex flex-row flex-wrap items-center justify-center gap-14">
 			{#each companyLogosUrl as companyLogoUrl}
 				<img src={companyLogoUrl} alt="Company Logo" />
